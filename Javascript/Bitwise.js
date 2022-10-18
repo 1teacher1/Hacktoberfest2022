5708b5143fd7a806633e7ea7d4b27b18c86ed067
Ashish <lit2021025@iiitl.ac.in>
2022-10-18 10:45:37 +0530

    Added Js code for Bitwise operator

---
+++ Javascript/Bitwise.js
@@ -0,0 +1,18 @@
+// Bitwise in JavaScript
+
+let x=5,y=12;
+// And(&) operator -> it sets each bit to 1 if both bits are 1
+
+console.log(x&y);
+
+// Or(|) operator -> It sets each bit to 1 if any of the bit is 1 
+
+console.log(x|y);
+
+// Xor(^) operator -> It sets all bit to 1 iff 1 bit is 1 and another is 0
+
+console.log(x^y);
+
+// Not(~) operator -> It reverts the Bit
+
+console.log(~x);
