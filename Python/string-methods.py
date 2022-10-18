6acf8107acfc385853c4642703ee5e079cf34582
Jeff Trojan <jefdauda@gmail.com>
2022-10-18 15:08:51 +0200

    Python/string-methods examples

--- Python/string-methods.py
+++ Python/string-methods.py
@@ -2,3 +2,21 @@ mystring = "this is the string"
 
 # To capitalize the first letter of the string
 print(mystring.capitalize())
+
+# To make the string in all uppercase
+print(mystring.upper())
+
+# To make the string in all lowercase
+print(mystring.lower())
+
+# To swap cases
+print(mystring.swapcase())
+
+# To convert the first character of each word to upper case
+print(mystring.title())
+
+# To return a tuple where the string is parted into three parts
+print(mystring.partition('the'))
+
+# To split the string at line breaks and returns a list
+print(mystring.splitlines())
